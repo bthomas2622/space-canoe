a74e39d7315961a8473f62fc3750be7c14068ed6
Ben Thomas <Ben Thomas>
2016-12-08 20:02:08 -0800

    changed rectangle sprites to box2d physics objects, able to rotate canoe with key presses

--- core/src/io/github/bthomas2622/GameScreen.java
+++ core/src/io/github/bthomas2622/GameScreen.java
@@ -2,15 +2,22 @@ package io.github.bthomas2622;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 import java.util.Iterator;
@@ -27,10 +34,14 @@ public class GameScreen implements Screen {
     Sound collisionSound;
     Music backgroundMusic;
     OrthographicCamera camera;
-    Rectangle canoe;
-    Array<Rectangle> spaceDebris;
+    Sprite canoe;
+    Array<Sprite> spaceDebris;
     long lastDebrisTime;
     int debrisDodged;
+    World world;
+    Body canoeBody;
+    Body debrisBody;
+    float torque = 0.0f;
 
     public GameScreen(final SpaceCanoe gam) {
         this.game = gam;
@@ -49,27 +60,34 @@ public class GameScreen implements Screen {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1280, 720);
 
-        // create a Rectangle to logically represent the canoe
-        canoe = new Rectangle();
-        canoe.x = 1280 / 2 - 300 / 2; // center the canoe horizontally
-        canoe.y = 720 / 2 - 100 / 2; // bottom left corner of the canoe is centered
-        // the bottom screen edge
-        canoe.width = 300;
-        canoe.height = 100;
+        // creating canoe sprite
+        canoe = new Sprite(canoeImage);
+        canoe.setPosition(Gdx.graphics.getWidth()/2 - canoe.getWidth() / 2, Gdx.graphics.getHeight() / 2 - canoe.getHeight() / 2);
+
+        //phyiscs world and bodytypes
+        world = new World(new Vector2(0, 0f), true);
+        BodyDef canoeBodyDef = new BodyDef();
+        BodyDef debrisBodyDef = new BodyDef();
+        canoeBodyDef.type = BodyDef.BodyType.KinematicBody;
+        debrisBodyDef.type = BodyDef.BodyType.DynamicBody;
+        canoeBodyDef.position.set(canoe.getX(),canoe.getY());
+        //create body in world using our definition
+        canoeBody = world.createBody(canoeBodyDef);
+        //define dimensions of the canoe physics shape
+        PolygonShape canoeShape = new PolygonShape();
+        canoeShape.setAsBox(canoe.getWidth()/2, canoe.getHeight()/2);
 
         // create the space debris array and spawn the first piece of debris
-        spaceDebris = new Array<Rectangle>();
+        spaceDebris = new Array<Sprite>();
         spawnDebris();
 
+        canoeShape.dispose();
     }
 
     //sets location for new space debris
     private void spawnDebris() {
-        Rectangle debris = new Rectangle();
-        debris.x = 1300;
-        debris.y = MathUtils.random(0, 720);
-        debris.width = 50;
-        debris.height = 50;
+        Sprite debris = new Sprite(spaceDebrisImage);
+        debris.setPosition(Gdx.graphics.getWidth() - debris.getWidth() / 2, Gdx.graphics.getHeight() / 2 - debris.getHeight() / 2);
         spaceDebris.add(debris);
         lastDebrisTime = TimeUtils.nanoTime();
     }
@@ -84,32 +102,35 @@ public class GameScreen implements Screen {
 
         // tell the camera to update its matrices.
         camera.update();
-
+        //step physics forward at rate of 60hx
+        world.step(1f/60f, 6, 2);
+        canoe.setPosition(canoeBody.getPosition().x, canoeBody.getPosition().y);
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+            canoe.setRotation((float)Math.toDegrees(30));
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+            canoe.setRotation((float)Math.toDegrees(60));
         // tell the SpriteBatch to render in the
         // coordinate system specified by the camera.
         game.batch.setProjectionMatrix(camera.combined);
+
+
 
         // begin a new batch and draw the canoe and
         // all drops
         game.batch.begin();
         game.font.draw(game.batch, "Debris Dodged: " + debrisDodged, 0, 480);
-        game.batch.draw(canoeImage, canoe.x, canoe.y, canoe.width, canoe.height);
-        for (Rectangle debris : spaceDebris) {
-            game.batch.draw(spaceDebrisImage, debris.x, debris.y);
+        game.batch.draw(canoe, canoe.getX(), canoe.getY(), canoe.getOriginX(), canoe.getOriginY(), canoe.getWidth(), canoe.getHeight(), canoe.getScaleX(), canoe.getScaleY(), canoe.getRotation());
+        for (Sprite debris : spaceDebris) {
+            game.batch.draw(debris, debris.getX(), debris.getY());
         }
         game.batch.end();
 
-        for (Rectangle debris : spaceDebris) {
-            if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
-                debris.y -= 200 * Gdx.graphics.getDeltaTime();
-            if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
-                debris.y += 200 * Gdx.graphics.getDeltaTime();
-        }
-
-//        if (Gdx.input.isKeyPressed(Input.Keys.UP))
-//            canoe.x -= 200 * Gdx.graphics.getDeltaTime();
-//        if (Gdx.input.isKeyPressed(Input.Keys.DOWN))
-//            bucket.x += 200 * Gdx.graphics.getDeltaTime();
+//        for (Sprite debris : spaceDebris) {
+//            if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+//                debris.y -= 200 * Gdx.graphics.getDeltaTime();
+//            if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+//                debris.y += 200 * Gdx.graphics.getDeltaTime();
+//        }
 
 //        // make sure the bucket stays within the screen bounds
 //        if (bucket.x < 0)
@@ -123,20 +144,20 @@ public class GameScreen implements Screen {
 
         // move the debris, remove any that are beyond edge of
         // the screen or that hit the canoe. iterate counter and add sound effect
-        Iterator<Rectangle> iter = spaceDebris.iterator();
-        while (iter.hasNext()) {
-            Rectangle debris = iter.next();
-            debris.x -= 200*Gdx.graphics.getDeltaTime();
-            if (debris.x < 0){
-                iter.remove();
-                debrisDodged++;
-            }
-            if (debris.overlaps(canoe)) {
-                //launch game over screen
-                //collisionSound.play();
-                iter.remove();
-            }
-        }
+//        Iterator<Sprite> iter = spaceDebris.iterator();
+//        while (iter.hasNext()) {
+//            Sprite debris = iter.next();
+//            debris.x -= 200*Gdx.graphics.getDeltaTime();
+//            if (debris.x < 0){
+//                iter.remove();
+//                debrisDodged++;
+//            }
+//            if (debris.overlaps(canoe)) {
+//                //launch game over screen
+//                //collisionSound.play();
+//                iter.remove();
+//            }
+//        }
     }
 
     @Override
@@ -166,6 +187,7 @@ public class GameScreen implements Screen {
     public void dispose() {
         canoeImage.dispose();
         spaceDebrisImage.dispose();
+        world.dispose();
         //collisionSound.dispose();
         //paddleSound.dispose();
         //backgroundMusic.dispose();
