5a65f6818c2ed5b650707c58f71b5a0781f27bf0
Ben Thomas <bthomas2622@gmail.com>
2016-12-17 13:52:57 -0500

    title image added to main menu screen

--- core/src/io/github/bthomas2622/MainMenuScreen.java
+++ core/src/io/github/bthomas2622/MainMenuScreen.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 /**
@@ -17,17 +19,18 @@ public class MainMenuScreen implements Screen {
     final SpaceCanoe game;
     OrthographicCamera camera;
     BitmapFont gameFont;
+    Texture spaceCanoeImage;
+    Sprite headerImage;
 
     public MainMenuScreen(final SpaceCanoe gam){
         game = gam;
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1280, 720);
-//        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("SpaceMono-Bold.ttf"));
-//        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-//        parameter.size = 20;
-//        gameFont = generator.generateFont(parameter);
-//        game.font = gameFont;
-//        generator.dispose(); //dispose generator to avoid memory leaks
+        spaceCanoeImage = new Texture(Gdx.files.internal("SpaceCanoe800w.png"));
+        headerImage = new Sprite(spaceCanoeImage);
+        headerImage.setPosition(Gdx.graphics.getWidth()/2 - headerImage.getWidth() / 2, Gdx.graphics.getHeight() - headerImage.getHeight() - 50);
+        headerImage.setOriginCenter();
+        headerImage.setRotation(0f);
     }
 
     @Override
@@ -49,6 +52,7 @@ public class MainMenuScreen implements Screen {
         game.batch.begin();
         gameFont.draw(game.batch, "PRESS 'ENTER'", Gdx.graphics.getWidth()/2 - enterWidth/2, Gdx.graphics.getHeight()/4);
         //game.font.draw(game.batch, "PRESS 'ENTER'", 600, 300);
+        game.batch.draw(headerImage, headerImage.getX(), headerImage.getY(), headerImage.getOriginX(), headerImage.getOriginY(), headerImage.getWidth(), headerImage.getHeight(), headerImage.getScaleX(), headerImage.getScaleY(), headerImage.getRotation());
         game.batch.end();
 
         if (Gdx.input.isKeyPressed(Input.Keys.ENTER)){
